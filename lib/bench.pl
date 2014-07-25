fe7638dd6bb6986b44a0d3d159818f4297c8884c
Eric Daniels <eric@erdaniels.com>
2014-07-25 19:29:52 -0400

    Added usage and optional profiling via NYTProf

--- lib/bench.pl
+++ lib/bench.pl
@@ -2,106 +2,158 @@ use 5.008;
 use strict;
 use warnings;
 use IO::Handle qw//;
-
+use Getopt::Long;
 use Benchmark qw/:all/;
+use Devel::NYTProf;
 use JSON::XS;
+use Pod::Usage;
 
 use MongoDB;
 
+my $cwd;
+{
+    use Cwd qw/abs_path/;
+    use Path::Class qw/file/;
+    $cwd = file(abs_path($0))->dir;
+}
+
+my $profile = '';
+my $dataset_path = '';
+
+=head1 SYNOPSIS
+
+bench.pl [options]
+
+Options:
+    -profile            enable profiling
+    -dataset            dataset to use
+
+=cut
+GetOptions(
+    "profile" => \$profile,
+    "dataset=s" => \$dataset_path,
+) or pod2usage(2);
+
+pod2usage(-verbose => 0, -message => "$0: dataset argument required\n") unless $dataset_path;
+
 sub get_data_from_json {
 
-	die "usage: get_twitter_data <file_path> <line_limit>" unless @_ == 2;
-	my $file_path = $_[0];
-	my $line_limit = $_[1] == -1 ? 0+'inf' : $_[1];
+    die "usage: get_twitter_data <file_path> <line_limit>" unless @_ == 2;
+    my $file_path = $_[0];
+    my $line_limit = $_[1] == -1 ? 0+'inf' : $_[1];
 
-	open(my $fh, "<:encoding(UTF-8)", $file_path) or die("Can't open $file_path: $!\n");
-	my @docs;
-	for (my $line_num = 0, my $line = <$fh>; $line_num < $line_limit && $line; $line = <$fh>, $line_num++) {
+    open(my $fh, "<:encoding(UTF-8)", $file_path) or die("Can't open $file_path: $!\n");
+    my @docs;
+    for (my $line_num = 0, my $line = <$fh>; $line_num < $line_limit && $line; $line = <$fh>, $line_num++) {
 
-		push @docs, decode_json($line);
-	}
-	return @docs;
+        push @docs, decode_json($line);
+    }
+    return @docs;
 }
 
 sub create_dataset {
 
-	my $data = $_[0];
+    my $data = $_[0];
 
-	my %dataset = (
+    my %dataset = (
 
-		all => $data,
-		single_doc => $data->[0],
-		docs_100 => [@$data[1..100]],
-		docs_1000 => [@$data[1..1000]],
-	);
+        all => $data,
+        single_doc => $data->[0],
+        docs_100 => [@$data[1..100]],
+        docs_1000 => [@$data[1..1000]],
+    );
 
-	return %dataset;
+    return %dataset;
 }
 
-my @tweets = get_data_from_json('twitter.json', 10_000);
+my @tweets = get_data_from_json($dataset_path, 2_000);
 my %dataset = create_dataset(\@tweets);
 my %scheme = (
 
-	find_spec => {source=>"web"}
+    find_spec => {source=>"web"}
 );
 
 my $client = MongoDB::MongoClient->new;
 my $db = $client->get_database("benchdb");
 
+sub profile {
+
+    return &{$_[0]} unless $profile;
+    DB::enable_profile;
+    &{$_[0]};
+    DB::disable_profile;
+}
+
 # Fill db for read with dataset
 {
-	print "Importing dataset...";
-	STDOUT->flush();
+    print "Importing dataset...";
+    STDOUT->flush();
 
-	my $read_coll = $db->get_collection("read");
-	$read_coll->drop;
-	$read_coll->insert($_) for @{$dataset{all}};
-	print " done.\n";
+    my $read_coll = $db->get_collection("read");
+    $read_coll->drop;
+    $read_coll->insert($_) for @{$dataset{all}};
+    print " done.\n";
 }
 
+# Creating
 {
-	my $insert_coll = $db->get_collection("inserts");
+    my $insert_coll = $db->get_collection("inserts");
 
-	timethese( -2, {
+    timethese( -2, {
 
-		"single insert" => sub {
+        "single insert" => sub {
 
-			$insert_coll->insert($dataset{single_doc});
-		},
+            profile( sub { $insert_coll->insert($dataset{single_doc}) } );
+        },
 
-		"batch insert 100" => sub {
+        "batch insert 100" => sub {
 
-			$insert_coll->insert($dataset{docs_100});
-		},
+            profile( sub { $insert_coll->insert($dataset{docs_100}) } );
+        },
 
-		"batch insert 1000" => sub {
+        "batch insert 1000" => sub {
 
-			$insert_coll->insert($dataset{docs_1000});
-		}
-	});
+            profile( sub { $insert_coll->insert($dataset{docs_1000}) } );
+        }
+    });
 
-	$insert_coll->drop;
+    $insert_coll->drop;
 }
 
+# Reading
 {
-	my $read_coll = $db->get_collection("read");
+    my $read_coll = $db->get_collection("read");
 
-	timethese( -2, {
+    timethese( -2, {
 
-		"find_one simple" => sub { $read_coll->find_one; },
+        "find_one simple" => sub {
+            profile( sub { $read_coll->find_one } );
+        },
 
-		"find_one query" => sub { $read_coll->find_one($scheme{find_spec}); },
+        "find_one query" => sub {
+            profile( sub { $read_coll->find_one($scheme{find_spec}) } );
+        },
 
-		"find all and iterate" => sub { 
-			my $cursor = $read_coll->find();
-			() while $cursor->next;
-		},
+        "find all and iterate" => sub { profile( sub {
 
-		"find on query and iterate" => sub { 
-			my $cursor = $read_coll->find($scheme{find_spec});
-			() while $cursor->next;
-		}
-	});
+            my $cursor = $read_coll->find();
+            () while $cursor->next;
+        })},
+
+        "find on query and iterate" => sub { profile( sub {
+
+            my $cursor = $read_coll->find($scheme{find_spec});
+            () while $cursor->next;
+        })},
+    });
 }
 
-$db->get_collection("read")->drop;
+# Updating
+
+# Deleting
+
+END {
+
+    DB::finish_profile if $profile;
+    $db->get_collection("read")->drop if $db;
+}
